95648f31d585bae66d522698343a421a2f7e57fa
shequin-joshua-9147 <joshua.shequin.9147@mail.linnbenton.edu>
2019-11-24 14:19:54 -0800

    Started server.js file serving. Currently statically serves all files in public, defaults the website to index.html, and if a requested file does not exist it responds with 404.html. The server port by default is 8000, unless the PORT environment variable exists, then it uses that.

---
+++ server.js
@@ -0,0 +1,49 @@
+/* Server javascript file for our final project: Rock-paper-scissors site
+ *
+ *
+ *
+ *
+ */
+
+// Imports
+
+var express = require('express');
+var process = require('process');
+
+// End Imports
+
+
+//////Start/////////////--File Hosting--///////////////////////////////////////////////
+
+var app = express();
+
+app.get('/', function(req, res, next){
+	// set our default page to index.html
+	res.status(200).sendFile(__dirname + "/public/index.html");
+});
+
+
+express.use(express.static('public')); // any files in public can be requested and will be returned.
+
+app.get('*', function (req, res, next){
+	// if the file requested does not have a get setup or is not static we send the 404.html page
+	// and the status of 404. 
+	// ATM 404.html doesn't exist.
+	res.status(404).sendFile(__dirname + "/public/404.html");
+});
+
+//////End///////////////--File Hosting--///////////////////////////////////////////////
+
+/////--SERVER START--//////////////////////////
+
+SERVER_PORT = process.env.PORT; // this is the port that the server will listen on.
+// set with PORT environment variable.
+
+if (SERVER_PORT == undefined){
+	// If the PORT variable does not exist, default to port 3000
+	SERVER_PORT = 8000;
+}
+
+app.listen(SERVER_PORT, function (){
+	console.log("Server has started and is listening on port " + SERVER_PORT);
+})
